dfbfeafb5a604fc7938ea09123b97acf6958bcaa
Ubuntu <ubuntu@ip-172-31-44-15.ec2.internal>
2015-09-24 00:12:38 +0000

    kevin

--- code/dev/cosine_tuning.R
+++ code/dev/cosine_tuning.R
@@ -1,7 +1,9 @@
-setwd('/media/hdd/kaggle/recruit/data')
-source('/media/hdd/kaggle/recruit/code/dev/data_process.R')
-source('/media/hdd/kaggle/recruit/code/dev/evaluation-mapk.R')
-
+#setwd('/media/hdd/kaggle/recruit/data')
+#source('/media/hdd/kaggle/recruit/code/dev/data_process.R')
+#source('/media/hdd/kaggle/recruit/code/dev/evaluation-mapk.R')
+setwd('/home/ubuntu/JOE/data')
+source('/home/ubuntu/JOE/recruit/code/dev/data_process.R')
+source('/home/ubuntu/JOE/recruit/code/dev/evaluation-mapk.R')
 
 library(doParallel) # Used to compute the user matrix
 library(data.table) # Used to Rbind the results 
@@ -23,10 +25,10 @@ W <- as.matrix(Diagonal(x=c(rep(2,13), rep(2,1), rep(-0.13,1), rep(0,9), rep(0.5
 
 tuner <- function(){
   for (WK in 1:length(wk_pairs)){
-    getData('cos_sim_ser', wk_pairs[[WK]][1], wk_pairs[[WK]][2])
+    getData('cos_sim_ser_female', wk_pairs[[WK]][1], wk_pairs[[WK]][2])
     
-    source('/media/hdd/kaggle/recruit/code/dev/model_process_cst.R')
-    
+#    source('/media/hdd/kaggle/recruit/code/dev/model_process_cst.R')
+    source('/home/ubuntu/JOE/recruit/code/dev/model_process_cst.R')   
     # validation_coupon_index
     # formatted_validation_set
     # val_removed_training_data
@@ -36,7 +38,7 @@ tuner <- function(){
     # all_testing_data
     # all_testing_coupon_ids
     
-    sample_iterations <-10
+    sample_iterations <- 40
     
     try(feature_weights <- mclapply(1:sample_iterations, cosine_tuner2, R_TARGETS, R_NT, mc.cores = 7))
     try(D <- return_weights(feature_weights, paste('V', 1:ncol(A), sep='')))
